4d19ae92fcac0a76db6300a9adf1808bcabbc44b
msmith9393 <megan@tokaypress.com>
2016-03-18 17:16:47 -0700

    change app and video list entry to classes

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,33 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer firstVideo={exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList videoData={exampleVideoData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  // handleClick() {
+  //   this.setState({
+
+  //   });
+  // }
+  
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer firstVideo={this.state.currentVideo} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videoData={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
+} 
 
 
 
--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,14 +1,44 @@
-var VideoListEntry = (props) => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
-      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
-    </div>
-  </div>
-);
+class VideoListEntry extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  handleClick() {
+    console.log('hi');
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry">
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div onClick={this.handleClick.bind(this)} className="video-list-entry-title">{this.props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+        </div>
+      </div>
+    );
+  }
+} 
+
 
 window.VideoListEntry = VideoListEntry;
+
+
+
+// var VideoListEntry = (props) => (
+//   <div className="video-list-entry">
+//     <div className="media-left media-middle">
+//       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+//     </div>
+//     <div className="media-body">
+//       <div onClick={this.handleClick.bind(this)} className="video-list-entry-title">{props.video.snippet.title}</div>
+//       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+//     </div>
+//   </div>
+// );
+
+// window.VideoListEntry = VideoListEntry;
 
